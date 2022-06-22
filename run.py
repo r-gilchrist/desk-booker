dcfc0816f89d7af539a0ad01a46508195e62f40d
Ryan Gilchrist <56300878+r-gilchrist@users.noreply.github.com>
2022-06-22 23:13:51 +0100

    Initialised simple server setup

---
+++ run.py
@@ -0,0 +1,3 @@
+from booker import app
+
+app.run()
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="booker",
+    version="0.1",
+    packages=["booker"],
+    url="https://github.com/r-gilchrist/desk-booker",
+    licence="MIT",
+    author="r-gilchrist",
+    author_email="ryangilchrist92@outlook.com",
+    description="Flask API for booking desks"
+)
