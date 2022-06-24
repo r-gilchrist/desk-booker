8d115ff6c40f5b3fc94bb2df5f5f9b5f7d276bbc
Ryan Gilchrist <56300878+r-gilchrist@users.noreply.github.com>
2022-06-24 22:23:10 +0100

    Added basic GET, POST, DELETE
    No tests yet, just quickly pulling an idea together for now.

--- booker/app.py
+++ booker/app.py
@@ -1,21 +1,33 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
+from .utils import database
+
+database.ensure_tables_are_created()
 
 app = Flask(__name__)
 api = Api(app)
 
 class Desk(Resource):
 
-    def get(self):
-        return {}, 200
+    def get(self, desk_id):
+        bookings = database.get_bookings(desk_id)
+        if len(bookings) == 0:
+            return {"msg": f"No bookings for desk {desk_id}!"}, 203
+        
+        ids, _, names = zip(*bookings)
+        return {id: {"name": name} for (id, name) in zip(ids, names)}
 
-    def post(self):
+    def post(self, desk_id):
+        data = request.get_json()
+        database.add_booking(desk_id, data["name"])
         return {}, 201
 
-    def delete(self):
+    def delete(self, desk_id):
+        database.delete_bookings(desk_id)
         return {}, 202
 
-api.add_resource(Desk, "/desk")
+
+api.add_resource(Desk, "/desk/<string:desk_id>")
 
 def run():
     app.run(debug=True)
---
+++ booker/utils/database.py
@@ -0,0 +1,49 @@
+import sqlite3
+import os
+
+FILENAME = "database.db"
+
+
+def ensure_tables_are_created():
+    con = sqlite3.connect(FILENAME)
+    cur = con.cursor()
+
+    cur.execute('''CREATE TABLE IF NOT EXISTS desk
+               (id INTEGER PRIMARY KEY AUTOINCREMENT, 
+               desk_id text NOT NULL, person text NOT NULL)''')
+
+    con.commit()
+    con.close()
+
+
+def get_bookings(desk_id):
+    con = sqlite3.connect(FILENAME)
+    cur = con.cursor()
+    result = cur.execute(f"SELECT * FROM desk WHERE desk_id = '{desk_id}'").fetchall()
+    con.close()
+    return result
+
+
+def add_booking(desk_id, person):
+    con = sqlite3.connect(FILENAME)
+    cur = con.cursor()
+    cur.execute("INSERT INTO desk(desk_id, person) VALUES(?, ?)", [desk_id, person])
+    id = cur.lastrowid
+    con.commit()
+    con.close()
+    return id
+
+
+def delete_bookings(desk_id):
+    con = sqlite3.connect(FILENAME)
+    cur = con.cursor()
+    result = cur.execute("DELETE FROM desk where desk_id = ?", (desk_id,))
+    deleted = result.rowcount
+    con.commit()
+    con.close()
+    return deleted == 1
+
+
+def get_db_status():
+    # It's always running perfectly, as long as it exists of course! :)
+    return os.path.exists(FILENAME)
