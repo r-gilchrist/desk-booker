b7baf7c40f6cb7945df20a3f0d105bded8b1e989
Ryan Gilchrist <56300878+r-gilchrist@users.noreply.github.com>
2022-06-24 21:42:53 +0100

    Added flask restful /desk routes
    GET, POST, DELETE

--- booker/app.py
+++ booker/app.py
@@ -1,6 +1,21 @@
 from flask import Flask
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
+
+class Desk(Resource):
+
+    def get(self):
+        return {}, 200
+
+    def post(self):
+        return {}, 201
+
+    def delete(self):
+        return {}, 202
+
+api.add_resource(Desk, "/desk")
 
 def run():
-    app.run()
+    app.run(debug=True)
