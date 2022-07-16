d900afbb503c2ee07bc1a3de80455147d0cc8695
Ryan Gilchrist <56300878+r-gilchrist@users.noreply.github.com>
2022-07-16 18:38:04 +0100

    Refactored http Responses

--- booker/app.py
+++ booker/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
 from .utils import database
+from .utils import httpResponses
 
 database.ensure_tables_are_created()
 
@@ -11,20 +12,29 @@ api = Api(app)
 class Desk(Resource):
 
     def get(self, desk_id):
+
         bookings = database.get_bookings(desk_id)
+
         if len(bookings) == 0:
-            return {"msg": f"No bookings for desk {desk_id}!"}, 203
+            return {"msg": f"No bookings for desk {desk_id}!"}, httpResponses.NO_BOOKINGS
+
         ids, _, names = zip(*bookings)
-        return {id: {"name": name} for (id, name) in zip(ids, names)}
+
+        return {id: {"name": name} for (id, name) in zip(ids, names)}, httpResponses.GET_OK
 
     def post(self, desk_id):
+
         data = request.get_json()
+
         database.add_booking(desk_id, data["name"])
-        return {}, 201
+
+        return {}, httpResponses.POST_OK
 
     def delete(self, desk_id):
+
         database.delete_bookings(desk_id)
-        return {}, 202
+
+        return {}, httpResponses.DELETE_OK
 
 
 api.add_resource(Desk, "/desk/<string:desk_id>")
