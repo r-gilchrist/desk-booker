c44dddaa9f02fae0ff4e034a44b797f5b021e63a
Ryan Gilchrist <56300878+r-gilchrist@users.noreply.github.com>
2022-07-18 21:29:04 +0100

    Refactored shifts.py

--- booker/utils/shifts.py
+++ booker/utils/shifts.py
@@ -2,9 +2,6 @@ import requests
 
 BASE_URL = "http://127.0.0.1:5000/desk/"
 
-DATE = "2022-08-01"
-
-VALID_DESKS = [f"S{str(n).zfill(3)}" for n in range(1, 101)]
-
-for desk_id in VALID_DESKS:
-    requests.post(BASE_URL + desk_id, json={"name": "unassigned", "date": DATE})
+def add_shifts(desk_ids, date):
+    for desk_id in desk_ids:
+        requests.post(BASE_URL + desk_id, json={"name": "unassigned", "date": date})
